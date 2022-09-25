05b6512a6bf76518e1aa451301906701663c5d2b
Zoibderg <43228316+Zoibderg@users.noreply.github.com>
2022-09-25 12:38:00 -0700

    some more minor exploration of ergast-py

--- explore_ergastpy.py
+++ explore_ergastpy.py
@@ -3,6 +3,11 @@ This file is for exploring fastf1 package. It seems to offer us more options in 
 of exploring and managing the data from the ergast API.
 """
 
+"""
+This is pretty advanced and there is a LOT that can be done. I will want to
+keep exploring deeper to get more comfortable with this package. 
+"""
+
 import fastf1
 from fastf1 import plotting
 from matplotlib import pyplot as plt
@@ -26,4 +31,13 @@ ax.plot(ham['LapNumber'], ham['LapTime'], color='cyan')
 ax.set_title("LEC vs HAM")
 ax.set_xlabel("Lap Number")
 ax.set_ylabel("Lap Time")
-plt.show()
+# plt.show()
+
+# SHOW 2022 RACES, INCLUDING TESTING
+event = fastf1.get_event_schedule(2022)
+#print(event)
+
+miami_gp = fastf1.get_session(2022, 'Miami', 'R')
+miami_gp.load()
+results = miami_gp.results   # this is a pandas dataframe
+print(results)
