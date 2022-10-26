f43b7fdf7a86de23ffd68b820231694bb857257e
Zoibderg <43228316+Zoibderg@users.noreply.github.com>
2022-10-26 13:02:13 -0700

    added pull all gp winners to ergast explore

--- explore_ergastpy.py
+++ explore_ergastpy.py
@@ -4,11 +4,10 @@ of exploring and managing the data from the ergast API.
 """
 
 
-
 from unittest import result
 import matplotlib.pyplot as plt
 import pandas as pd
-import pprint
+import pprint as pp
 from timple.timedelta import strftimedelta
 import fastf1
 import fastf1.plotting as plotting
@@ -20,46 +19,53 @@ plotting.setup_mpl()
 
 fastf1.Cache.enable_cache('./cache')  # optional but recommended
 
-race = fastf1.get_session(2020, 'Turkish Grand Prix', 'R')
-race.load()
+# race = fastf1.get_session(2020, 'Turkish Grand Prix', 'R')
+# race.load()
 
-lec = race.laps.pick_driver('LEC')
-ham = race.laps.pick_driver('HAM')
+# lec = race.laps.pick_driver('LEC')
+# ham = race.laps.pick_driver('HAM')
 
-# PLOTTING DATA
-fig, ax = plt.subplots()
-ax.plot(lec['LapNumber'], lec['LapTime'], color='red')
-ax.plot(ham['LapNumber'], ham['LapTime'], color='cyan')
-ax.set_title("LEC vs HAM")
-ax.set_xlabel("Lap Number")
-ax.set_ylabel("Lap Time")
+# # PLOTTING DATA
+# fig, ax = plt.subplots()
+# ax.plot(lec['LapNumber'], lec['LapTime'], color='red')
+# ax.plot(ham['LapNumber'], ham['LapTime'], color='cyan')
+# ax.set_title("LEC vs HAM")
+# ax.set_xlabel("Lap Number")
+# ax.set_ylabel("Lap Time")
 # plt.show()
 
-# SHOW 2022 RACES, INCLUDING TESTING
+# SHOW 2022 RACES, EXCLUDING TESTING
 events = fastf1.get_event_schedule(2022, include_testing=False)
-print(events)
+# print(events)
 
 
-# eventnames = []
-# winners22 = {}
-# for event in events['OfficialEventName']:
-#     eventname = str(event)
-#     eventnames.append(eventname)
+winners22 = {}
 
-# for name in eventnames:
-#     gp = fastf1.get_session(2022, name, 'R')
-#     gp.load()
-#     result = gp.results
-#     print(result)
-#     try:
-#         winner = result['BroadcastName'].iloc[0]
-#         winners22[name] = winner
-#     except Exception:
-#         winners22[name] = 'TBD'
+eventnames = [f"Round.{round}: \
+-- {events[events['RoundNumber'] == round]['Location'].values[0]} -- \
+{events[events['RoundNumber'] == round]['OfficialEventName'].values[0]}" 
+for round in events['RoundNumber']]
 
-# pprint.pprint(winners22)
+# print(eventnames)
 
-# FOR EACH EVENT, SHOW THE RACE RESULTS
+
+# GRAB WINNERS
+for name in eventnames:
+    gp = fastf1.get_session(2022, name, 'R')
+    gp.load()
+    result = gp.results
+    # print(result)
+    try:
+        winner = result['BroadcastName'].iloc[0]
+        winners22[name] = winner
+    except Exception:
+        winners22[name] = 'TBD'
+
+print(f"\nFORMULA 1 2022 GP WINNERS")
+print("----------------------------")
+for entry in winners22:
+    print(f"\n{entry.title()} : {winners22[entry]}")
+    print(f"\n-----------------------------")
 
 
 # miami_gp = fastf1.get_session(2022, 'Miami', 'R')
